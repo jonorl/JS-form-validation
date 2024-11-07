6d1294d4d932d9df9993302e90924aae467a8dbe
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-07 15:52:55 +0300

    Swapped logic to switch cases to start adding country, poscode, password and password confirm

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ import "../css/style.css";
 
 // Global variables
 
-const form = document.querySelector("form")
-const email = document.getElementById("email")
+const form = document.querySelector("form");
+const email = document.getElementById("email");
 let error;
 let targetID;
 
@@ -12,7 +12,7 @@ let targetID;
 form.addEventListener("input", (event) => {
   targetID = event.target.id;
   error = document.querySelector(`#${targetID} + span.error`);
-  switch(event.target){
+  switch (event.target) {
     case email:
       if (email.validity.valid) {
         error.textContent = ""; // Remove the message content
@@ -20,6 +20,7 @@ form.addEventListener("input", (event) => {
       } else {
         // If there is still an error, show the correct error
         showError(event, error);
+        break;
       }
   }
 });
@@ -29,17 +30,21 @@ form.addEventListener("submit", (event) => {
 });
 
 function showError(event, error) {
-  let target = event.target
-  if (target.validity.valueMissing) {
-    // If empty
-    error.textContent = "You need to enter an email address.";
-  } else if (email.validity.typeMismatch) {
-    // If it's not an email address,
-    error.textContent = "Entered value needs to be an email address.";
-  } else if (email.validity.tooShort) {
-    // If the value is too short,
-    error.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  let target = event.target;
+  switch (target.id) {
+    case "email":
+      if (target.validity.valueMissing) {
+        // If empty
+        error.textContent = "You need to enter an email address.";
+      } else if (email.validity.typeMismatch) {
+        // If it's not an email address,
+        error.textContent = "Entered value needs to be an email address.";
+      } else if (email.validity.tooShort) {
+        // If the value is too short,
+        error.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+      }
+      // Add the `active` class
+      error.className = "error active";
+      break;
   }
-  // Add the `active` class
-  error.className = "error active";
 }
